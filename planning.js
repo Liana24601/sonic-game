de106264c8b93409918d452c4afd8c150502cdfa
Liana H. <lianadothallik@gmail.com>
2025-07-27 18:51:53 -0400

    Create planning.js

---
+++ planning.js
@@ -0,0 +1,18 @@
+/*
+
+\\ GitHub readme \\
+
+Why Did You Make This Game?
+To pay homage to my deeply obsessive and addicting obsession with Sonic the Hedgehog, I have decided to create my very own Sonic the Hedgehog game.
+Whether it's the popular (and hilarious) Sonic movies, the Netflix show "Sonic Prime", the IDW comics, or the copious amounts of fan content that exists on the internet and social media platforms, I have always harbored a strong and fond interest for a singular character: Shadow the Hedgehog.
+With his compelling character arc, awesome design, "I don't care" attitude, and tear-worthy backstory, what's not to love about Shadow the Hedgehog, a character who almost always stole the show with his red highlights and air shoes.
+However, canon Shadow content is very limited. He barely got any screentime in Sonic Prime and Sonic Boom. Only one movie has been made on him so far. Sonic is often given importance over him.
+I was dissapaointed, as I did not get to see much of Shadow during shows and movies, and was too broke to afford the games. 
+Therefore, I decided to create the next biggest project of my life: A homemade video game starring Shadow the Hedgehog.
+"Sprinting to Sonic" is an intriguing maze-based game which delves deeply into the multifaceted and highly complex relationship between Shadow and Sonic. Two souls who are simultaneously polar opposite and very alike.
+The show "Sonic Prime" and the movie "Sonic 3" were huge inspirations for this concept, as both showed the hedgehogs' dynamics in fascinating yet deeply different aways.
+In "Sprinting to Sonic", I decided to take all the things I admire about the franchise and put it into a game. I poured my heart and soul into making this game, so I hope it's enjoyable!
+Thus, coding, storyboarding, and creating this game was a beautiful experience, as I was able to experiment and tamper with a single, compelling notion: what are the legnths Shadow would go to in order to see Sonic?
+The answer, to my surprise, turned out to be "everything."
+
+*/
