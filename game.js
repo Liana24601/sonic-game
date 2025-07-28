fa2f9cfbdf65bcb80f7bb00f09ef9f96baea239a
Liana H. <lianadothallik@gmail.com>
2025-07-27 22:44:05 -0400

    Create game.js

---
+++ game.js
@@ -0,0 +1,20 @@
+body {
+  margin: 0;
+  font-family: 'Segoe UI', sans-serif;
+  background-color: black;
+  color: white;
+  text-align: center;
+  overflow: hidden;
+}
+
+.scene {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+}
+
+.hidden {
+  display: none;
+}
